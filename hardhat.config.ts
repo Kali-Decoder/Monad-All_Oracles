4b2ff33d1b9588f4e7b261e721f05d6d8b25139a
Neeraj Choubisa <neerajchoubisa876@gmail.com>
2025-03-24 09:31:16 +0530

    update pyth contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,80 @@
-import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
 
-const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+const ACCOUNTS = process.env.DEPLOYER_ACCOUNT_PRIV_KEY
+  ? [`${process.env.DEPLOYER_ACCOUNT_PRIV_KEY}`]
+  : [];
+
+module.exports = {
+  defaultNetwork: "hardhat",
+  gasReporter: {
+    enabled: false,
+  },
+  networks: {
+    hardhat: { chainId: 31337 },
+    bnbTestnet: {
+      chainId: 97,
+      url: "https://bsc-testnet.public.blastapi.io",
+      accounts: ACCOUNTS,
+    },
+    bnbMainnet: {
+      chainId: 56,
+      url: "https://bsc.nodereal.io", // "https://binance.llamarpc.com", // "https://binance.llamarpc.com", // "https://bsc.drpc.org", // "https://bsc-pokt.nodies.app", // "https://binance.llamarpc.com",
+      accounts: ACCOUNTS,
+    },
+    xrpl_evm:{
+      chainId: 1440002,
+      url: "https://rpc-evm-sidechain.xrpl.org",
+      accounts: ACCOUNTS,
+    },
+    monadTestnet:{
+      chainId: 10143,
+      url: "https://monad-testnet.g.alchemy.com/v2/XvtthJ8YRg0riu9y90IdPJYSDm3HBtai",
+      accounts: ACCOUNTS,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      
+    },
+    customChains: [
+      {
+        network: "iotaEvmMainnet",
+        chainId: 8822,
+        urls: {
+          apiURL: "https://explorer.evm.iota.org/api",
+          browserURL: "https://explorer.evm.iota.org",
+        },
+      },
+      {
+        network: "monadTestnet",
+        chainId: 10143,
+        urls: {
+          apiURL: "https://explorer.evm.iota.org/api",
+          browserURL: "https://testnet.monadexplorer.com/",
+        },
+      },
+  
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      evmVersion: "paris",
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
 };
-
-export default config;
